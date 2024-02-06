a41ffb8dad0fb504afb48dfcc89b59bcc40a3f62
OLUWABORII <83953205+Oluwa-bowry@users.noreply.github.com>
2024-02-06 17:07:35 +0100

    embed images

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Predict the next Hall of Fame",
     description:
       "PREDICTING THE NEXT HALL OF FAME PLAYER USING LINEAR REGRESSION AND WEB SCRAPING ",
-    image: "https://photos.google.com/share/AF1QipNP3GYMBvKY6ucBlIs9DSzpi085q4blW1K6JUW41d6tTvf7ig060v_zY-pmujixyg/photo/AF1QipNvNEHMppiIKhAW6i9Yi4deQf06rZz-tlOVvb1o?key=YzVxc2tYeGk4dXNXbWFsV0pOMUNaYVgxbFhqN0Vn",
+    image: "https://lh3.googleusercontent.com/pw/ABLVV84uNHk39qMbxKPs5frfJ4vGt9gzlypx4qIQa2u1P0fMeMXQmd9lsekJkd2mlMITXnlahFS4EsE6PspKI0O8hBBJOgq9F4W5Ejh6sBnYdgzt2N6bkdM=w1920-h1080",
     link: "https://github.com/Oluwa-bowry/NBA_project",
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "Data scraping, wrangling and analysis",
     description:
       "Web scraping and analysis with python packages (selenium, scrapy, numpy, pandas, matplotlib and seaborn).",
-    image: "https://photos.google.com/share/AF1QipOAGBcteXpclCJTLPszkvFFJghrC21lbgUJUywz0jMIEoaz-yvvy-g3mOBPF9OoQA/photo/AF1QipOYVJcASrh-5Cysj-KorGoWpMDeLuYpRO4Whgvf?key=cW5KLXNuZWhRUFpFSHNBU1lCYXZaZ2Iyd0taczlR",
+    image: "https://lh3.googleusercontent.com/pw/ABLVV86gMl5x_SRx_UP9EC0VQD26GmAaGbWxYn33aljsZAmAWHSyJYnay_JNQjo4ebSE9GMfN7ZZRMcOQVbbdxPQZcIQ8Ir9aAg-V5nocCSOiA5ffHOfNDc=w1920-h1080",
     link: "https://drive.google.com/drive/folders/1SUSAI_gZGZDvbg81HZDvh6uZGLTc1PUg?usp=sharing",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "Speech recognition model in noisy environment",
     description:
       "To develop a machine learning techniques that can improve the robustness and accurracy in the presence of noise,",
-    image: "https://photos.google.com/share/AF1QipM2boBXnVpvM4Lid6e5gijv9utVnWqagGzKZ0ooY7Y70XftUjal8Z6jdPDsNeWt-w/photo/AF1QipPREiIMR6HT0XImTVgYRhs3IWipgw6YYYsb7-5D?key=NDJXMm56SzlUY2dxblhCX2FKMjVETjR0UlFKMUh3",
+    image: "https://lh3.googleusercontent.com/pw/ABLVV84STyzII-ipYL3N6XuQWiauQ4Bm6CoavtOSpqRspkTBwC_T0DUIoyYIx_ud87hTlcbBYMDEkg4Ga1lcMiIJoOx_2y9AgSjRSxnlx9zG1cDRFW-OE94=w1920-h1080",
     link: "https://drive.google.com/drive/folders/1vKRgnY5KR5WeizjvtXuiBzCWlhJTfEMv?usp=sharing",
   },
   {
